7d73ad11f1074927f5ec5ebc2e18991fe452d45b
hofman10 <nbijelic29@gmail.com>
2025-09-24 21:36:56 +0200

    Add routing and active link highlighting
    Implemented navigation routing to load specific HTML pages when links are clicked,
    and added active link styling to indicate the current page. Improves user experience
    and navigation clarity.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
 // apiKljuc = "f1dc3deee62bb7bf44211b2c5742952a";
 
-console.log(window.location.pathname);
+const global = {
+  currentPage: window.location.pathname,
+};
+
+//Highlight active link
+const highlightActiveLink = function () {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+};
+
+//Init App
+const init = function () {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home");
+      break;
+
+    case "/shows.html":
+      console.log("Shows");
+      break;
+
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+
+  highlightActiveLink();
+};
+
+window.addEventListener("DOMContentLoaded", init);
